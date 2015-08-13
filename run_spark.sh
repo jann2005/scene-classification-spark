e9847dedd0d65eedef73b5bad1fbc55b64ea130a
Jeffrey Tang <jeff@skymind.io>
2015-08-12 20:03:26 -0700

    Update run_spark.sh

--- run_spark.sh
+++ run_spark.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-
-# Building nd4j
-cd  ../nd4j
-git pull origin master
-mvn clean install -DskipTests
-
-# Building dl4j
-cd  ../deeplearning4j
-git pull origin master
-mvn clean install -DskipTests
-
-cd ../scene-classification-spark
-git pull origin master
-mvn clean install -DskipTests
-
 sh /root/spark/bin/spark-submit \
 --driver-memory 25G \
 --class org.deeplearning4j.App.java \
